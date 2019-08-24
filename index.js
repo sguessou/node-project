a0646465501322f1095e97d35adaa6a6744c05d1
Saad Guessous <guessous_saad@hotmail.com>
2019-08-24 17:31:06 +0300

    added billing route auth middleware

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-const privateKey = fs.readFileSync('./https/key.pem', 'utf8');
-const certificate = fs.readFileSync('./https/cert.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -27,13 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_ENV === 'development') {
-    const httpServer = http.createServer(app);
-    const httpsServer = https.createServer(credentials, app);
-    httpServer.listen(5000);
-    httpsServer.listen(5001);
-} else {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT);
-}
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
     googleId: String,
     githubId: String,
     linkedinId: String,
+    credits: { type: Number, default: 0 },
 });
 
 mongoose.model('users', userSchema);
